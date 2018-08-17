5e1c1d7cf8041fc0c96c71d79ec991b9c57d3bbe
bieradek <radoslaw.biedrzycki@syncron.com>
2018-08-17 16:27:38 +0200

    Initializing Sportscar

--- src/com/timbuchalka/Sportscar.java
+++ src/com/timbuchalka/Sportscar.java
@@ -1,4 +1,18 @@
 package com.timbuchalka;
 
-public class Sportscar extends  {
+public class Sportscar extends Car {
+
+    private boolean convertible;
+    private int drive;
+    private int exhaust;
+    private boolean loud;
+
+    //3rd level constructor
+    public Sportscar(String color, int size, int engine, boolean electric, boolean convertible, int drive, int exhaust, boolean loud) {
+        super(color, size, 2, 4, 20.5, engine, electric, 4);
+        this.convertible = convertible;
+        this.drive = drive;
+        this.exhaust = exhaust;
+        this.loud = loud;
+    }
 }
